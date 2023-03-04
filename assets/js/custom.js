6b73df6907894efc086c0116bf3acf4a86bdf199
xtian <benedictramirez78@gmail.com>
2023-03-04 16:03:16 +0800

    add active link //bug

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -20,3 +20,34 @@ let calcScrollValue = () => {
   window.onscroll = calcScrollValue;
   window.onload = calcScrollValue;
   
+
+  /**
+   * Navbar links active state on scroll
+   */
+  let navbarLinks = select('#navbarLinks .scrollto', true)
+  const navbarLinksActive = () => {
+    let position = window.scrollY + 200
+    navbarLinks.forEach(navbarLinks => {
+      if (!navbarLinks.hash) return
+      let section = select(navbarLinks.hash)
+      if (!section) return
+      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+        navbarLinks.classList.add('active')
+      } else {
+        navbarLinks.classList.remove('active')
+      }
+    })
+  }
+  window.addEventListener('load', navbarLinksActive)
+  onscroll(document, navbarLinksActive)
+
+  /**
+   * Scrolls to an element with header offset
+   */
+  const scrollto = (el) => {
+    let elementPos = select(el).offsetTop
+    window.scrollTo({
+      top: elementPos,
+      behavior: 'smooth'
+    })
+  }
